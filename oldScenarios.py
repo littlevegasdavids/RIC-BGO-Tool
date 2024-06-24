a71f8602547a678fc3d6ae2442184fe0c94089b9
Reno Davids <reno.davids@sovtech.com>
2024-06-24 16:00:26 +0200

    Check

--- oldScenarios.py
+++ oldScenarios.py
@@ -17,25 +17,20 @@ db_cur = db_conn.cursor()
 three_months_ago = datetime.now() - timedelta(days=90)
 three_months_ago_str = three_months_ago.strftime('%Y-%m-%d')
 
-# Correct SQL query
-query = 'SELECT id FROM public."Scenarios" WHERE upload_date < %s'
-
 # Executing the SQL query
-db_cur.execute(query, (three_months_ago_str,))
+db_cur.execute('SELECT id FROM public."Scenarios" WHERE upload_date < %s', (three_months_ago_str,))
 
 # Fetching the results - result is [(1,), (2,)]
 result = db_cur.fetchall()
 
 ids = [item[0] for item in result]
+id = ids[0]
 
-print(ids)
+print('Deleting the following id:', id)
+db_cur.execute('DELETE FROM public."Scenarios" WHERE id = %s', (id,))
 
-# deleteQuery = 'DELETE FROM public."Scenarios" WHERE id = %s'
-# print('Deleting the following ids:', ids[0])
-# db_cur.execute(deleteQuery, (ids[0],))
+# Closing the connection
+db_cur.close()
+db_conn.close()
 
-# # Closing the connection
-# db_cur.close()
-# db_conn.close()
-
-# print('Deleted ids:', ids[0])
+print('Deleted ids:', ids[0])
