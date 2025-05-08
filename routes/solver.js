5ed5a75881b2d9105ed4ede777458a8093b40298
Reno Davids <reno.davids@sovtech.com>
2025-05-08 08:36:31 +0200

    Added python requirements and point to venv python3 interpreter

--- routes/solver.js
+++ routes/solver.js
@@ -159,7 +159,7 @@ var solveScenario = async function (scenario_id, user_email, callback) {
     );
     writeStream.write(`### Scenario id: ${scenario_id} ###\n`);
 
-    py = pySpawn("python3", ["-u", pythonFileName, scenario_id]);
+    py = pySpawn("./venv/bin/python3", ["-u", pythonFileName, scenario_id]);
     await db.query(
       'UPDATE public."Scenarios" SET scenario_status = $1 WHERE id = $2',
       [1, scenario_id]
